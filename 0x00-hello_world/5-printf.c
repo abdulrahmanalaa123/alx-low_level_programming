5623d381ed3893ad02a5b7ebafc25a9aecbe5c3d
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-18 18:17:01 +0200

    Same message for automation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
