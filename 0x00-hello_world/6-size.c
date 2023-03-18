f218a43b258fc406774643c4053645ca8fcb9bae
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-18 18:40:46 +0200

    Same message for automation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char d;
 	float e;
 
-	printf("Size of a char: %lu byte(s)", sizeof(d));
-	printf("Size of an int: %lu byte(s)", sizeof(a));
-	printf("Size of a long int: %lu byte(s)", sizeof(b));
-	printf("Size of a long long int: %lu byte(s)", sizeof(c));
-	printf("Size of a float: %lu byte(s)", sizeof(e));
+	printf("Size of a char: %u byte(s)", sizeof(d));
+	printf("Size of an int: %u byte(s)", sizeof(a));
+	printf("Size of a long int: %u byte(s)", sizeof(b));
+	printf("Size of a long long int: %u byte(s)", sizeof(c));
+	printf("Size of a float: %u byte(s)", sizeof(e));
 	return (0);
 }
