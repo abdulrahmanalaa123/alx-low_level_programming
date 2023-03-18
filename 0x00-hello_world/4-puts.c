20537297d4db5f6e03eb8964896bce49d31e2b49
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-18 17:58:29 +0200

    Same message for automation

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *Description:4-puts "description for task"
+ *main: "enter description dependant on task"
+ *
+ *editing just for betty
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	/** 
+	 *Description:4-puts "description for task"
+	 */
+	puts("\"Programming is like solving a multilingual puzzle");
+	return (0);
+}
