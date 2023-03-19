e64ead429d779c6023e1055227a28e834c430bb5
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-19 15:12:25 +0200

    Same message for automation

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 1 as a default return value
+ */
+int main(void)
+{
+	system("echo -e 'and that piece of art is useful\"")
+	system("echo -e ' - Dora Korpar, 2015-10-19\n'")
+	return (1);
+}
