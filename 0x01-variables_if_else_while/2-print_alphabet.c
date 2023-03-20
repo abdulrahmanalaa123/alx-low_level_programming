fa53dd1dd1936bdaaa616aaa5123f9266f1d9756
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 03:32:55 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
