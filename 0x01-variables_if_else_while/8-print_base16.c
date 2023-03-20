bc3247f8b198b73ba0814ec3e2738c7b7de804cf
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 03:52:27 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+		putchar(letter);
+
+	char letter;
+
+	for (lette2 = 'a'; lette2 <= 'f'; lette2++)
+		putchar(lette2);
+
+	putchar('\n');
+
+	return (0);
+}
