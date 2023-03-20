9c1162a8406d00207065f1f73f64c97917102d4b
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 21:33:46 +0200

    Same message for automation

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,21 +8,20 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
-		for (j = i + 1; j <= 9; j++)
-			for (k = j + 1; k <= 9; k++)
+	for (i = 0; i < 8; i++)
+		for (j = i + 1; j < 9; j++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar(48 + i);
 				putchar(48 + j);
 				putchar(48 + k);
 
-				if (i == 8 && j == 9 && k == 7)
+				if (k == 9 && i == 7 && j == 8)
 					continue;
-
 				putchar(',');
 				putchar(' ');
+			
 			}
-
 	putchar('\n');
 
 	return (0);
