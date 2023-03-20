8c7d12a7cb68ef5b52ae6b2a7bc7d6bd97f1a3e0
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 09:50:20 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+		for (j = i + 1; j <= 9; j++)
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(48 + i);
+				putchar(48 + j);
+
+				if (i == 8 && j == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+
+	putchar('\n');
+
+	return (0);
+}
