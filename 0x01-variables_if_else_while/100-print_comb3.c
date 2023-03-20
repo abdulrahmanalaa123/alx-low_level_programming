88af315484abf9ad3b2bd70a7b3c648aa27a4a5c
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 09:45:00 +0200

    Same message for automation

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	char i;
-	char j;
+	int i,j;
 
-	for (i = '0'; i <= '9'; i++)
-		for (j = i + 1; j <= '9'; j++)
+	for (i = 0; i <= 9; i++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(i);
-			putchar(j);
+			putchar(48 + i);
+			putchar(48 + j);
 
-			if (i == '8' && j == '9')
+			if (i == 8 && j == 9)
 				continue;
 
 			putchar(',');
