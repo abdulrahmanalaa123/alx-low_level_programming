6deacefcece79c6cc2d7ab3f2d2faecfc5d51c1e
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 03:49:53 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
