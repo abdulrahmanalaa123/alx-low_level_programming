1829ab4637c7b0f348a42f086667d2da3c71d80b
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 03:39:06 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' || letter != 'e')
+			putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
