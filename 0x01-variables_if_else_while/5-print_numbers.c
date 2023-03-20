b55abb74eb2d2aef2e6caec98508ca67957411d5
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-20 03:45:25 +0200

    Same message for automation

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+		printf("%d", letter);
+
+	printf("\n");
+
+	return (0);
+}
