0c3f6c189886d536ff8aec0a7b1ac163e5d7e8cc
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-21 13:20:12 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	int i;
+	char s[] = "_putchar"
+	for (i = 0; i < sizeof(s); i++)
+		_putchar(s[i])	
+	return (0);
+}
