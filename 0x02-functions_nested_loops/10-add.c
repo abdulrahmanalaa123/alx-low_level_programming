5d165747c41a36d3696f07b91ba420ca774cea9b
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 05:06:36 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *Description: add - description for task.
+ *@a:first int
+ *@b:first int
+ *Return: 0 as a default return value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
