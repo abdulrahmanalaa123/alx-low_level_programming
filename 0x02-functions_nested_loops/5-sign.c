704dc316d606cf99f7014997ff550f6e2edb7494
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:19:42 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *Description: print_sign - description for task.
+ *@c: int to be checked
+ *Return: 0 as a default return value
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+		return (1);
+	else if (c == 0)
+		return (0);
+	else
+		return (-1)
+}
