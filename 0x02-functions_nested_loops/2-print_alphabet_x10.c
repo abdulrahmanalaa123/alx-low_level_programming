046ef995081e6106e50c548bafa4e5fc4a2c43bd
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 02:58:46 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+void print_alphabet_x10(void);
+{
+	for (int i = 0; i < 10; i++)
+		for (char c = 'a'; c <= 'z'; c++)
+			_putchar(c)
+
+	_putchar('\n');
+}
