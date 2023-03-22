3bdefc09a5865df968fd79574cbbca153cca72d0
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:25:56 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *Description: _abs - description for task.
+ *@int : number
+ *Return: 0 as a default return value
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return(-c);
+
+	return (c);
+}
