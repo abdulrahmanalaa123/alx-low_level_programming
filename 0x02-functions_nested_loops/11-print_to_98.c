8773ce9977e82eacf8317b6af31699a920f04320
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 05:18:26 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *Description: print_to_98 - description for task.
+ *@n:integer to go to
+ *Return: 0 as a default return value
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n >= 10)
+			_putchar(48 + n / 10);
+		_putchar(48 + n % 10);
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n++;
+		else
+			n--;
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
