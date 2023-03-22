e68e4757473fe6895e222c30638e953d02933810
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:29:55 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *Description: print_last_digit - description for task.
+ *@c: integer to be printed
+ *Return: 0 as a default return value
+ */
+int print_last_digit(int c)
+{
+	_putchar(c % 10);
+	return (c % 10);
+}
