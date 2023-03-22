526447d3c17db93cfc55a11646aa8e2fc2231889
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:51:54 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *Description: jack_bauer - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(48 + i / 10);
+			_putchar(48 + i % 10);
+			_putchar(':');
+			_putchar(48 + j / 10);
+			_putchar(48 + j % 10);
+		}
+}
