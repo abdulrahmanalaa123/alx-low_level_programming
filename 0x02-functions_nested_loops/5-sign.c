f25565e451ade6209e80b0bed4b1aab4f22ad983
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:23:00 +0200

    Same message for automation

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,9 +7,18 @@
 int print_sign(int c)
 {
 	if (c > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
 	else if (c == 0)
+	{
+		_putchar('0');
 		return (0);
+	}
 	else
+	{
+		_putchar('-');
 		return (-1);
+	}
 }
