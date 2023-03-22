5f198379a30d7e6b696912ac147a747c238f25a8
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-22 03:15:56 +0200

    Same message for automation

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *Description: _islower - description for task.
+ *@c: the character to be checker
+ *Return: 0 as a default return value
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+
+	return (0);
+}
