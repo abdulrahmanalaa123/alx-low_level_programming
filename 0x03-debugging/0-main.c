89656fa5310ebd094c25383411e0405d60f3085f
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-25 02:33:55 +0200

    Same message for automation

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *Description: main - description for task.
+ *
+ *Return: 0 as a default return value
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_neagtive(i);
+	return (0);
+}
