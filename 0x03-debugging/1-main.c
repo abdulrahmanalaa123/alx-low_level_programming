185b22d0a0b593bc754192b7ec7211b6b5fa481f
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-25 02:53:58 +0200

    Same message for automation

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *Description: main - description for task.
+ *Return: 0 as a default return value
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
