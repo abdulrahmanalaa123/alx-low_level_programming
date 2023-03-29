198027baf0fff12d50d7df2636961e709e9c86f4
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-29 04:29:59 +0200

    Same message for automation

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *Description: print_rev - description for task.
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	i = 0;
+	while (*s[i])
+	{
+		i = i + 1;
+	}
+	for (n = i - 1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
