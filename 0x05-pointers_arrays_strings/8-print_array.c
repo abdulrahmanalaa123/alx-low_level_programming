ae1ee8d64065d9924237fe822ec93b4fa9efb711
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-29 19:25:45 +0200

    Same message for automation

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_array - prints n element of an array of integers
- * Owned by Bwave
- * @a: int to check
- * @n: int to check
+ * print_array - prints every other character of a string
+ *
+ * @a: str of choice
+ * @n: amount to print
  * Return: 0 is success
  */
 void print_array(int *a, int n)
 {
-	int x;
+	int i;
 
-	for (x = 0; x < n; x++)
-		if (x != n - 1)
-		printf("%d, ", a[x]);
+	for (i = 0; i <= n; i++)
+	{
+		if (i != n - 1)
+			printf("%d ", *(a + i);
 		else
-			printf("%d", a[x]);
-printf("\n");
+			printf("%d", *(a + i);
+	}
+	_putchar('\n');
 }
