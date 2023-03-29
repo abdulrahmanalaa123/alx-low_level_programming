d067c6757525bfd5578370108e577e4a8860bb76
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-03-29 02:59:44 +0200

    Same message for automation

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *Description: _strlen - description for task.
+ *@s: string
+ *Return : length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+	
+	while (s++)
+		len++;
+	
+	return (len);
+}
