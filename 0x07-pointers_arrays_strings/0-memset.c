ae857ad4db701c68b434b59858623eb105290eca
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-05 04:32:00 +0200

    Same message for automation

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  *Description: _memset - description for task.
- *@s: input string
- *@b: char to chagne
- *@n: number of changes
- *Return: 0 as a default return value
+ *@s : input string
+ *@b : char to chagne
+ *@n : number of changes
+ *Return: s as a default return value
  */
 char *_memset(char *s, char b, unsigned int n)
 {
