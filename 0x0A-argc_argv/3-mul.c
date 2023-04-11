2c6152aa7c2a58e94134ce1eb77122bed786e455
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-11 12:52:22 +0200

    Same message for automation

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,58 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int _atoi(char *str);
 /**
- *Description: main - description for task.
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments
  *
- *@argc: number of arguments for func
- *@argv: arguments
- *
- *Return: 0 as a default return value
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+	int x, y;
+
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int mult;
-	int i;
-	int k;
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
-	mult = 1;
-	for (i = 1; i < argc; i++)
-	{
-		k = _atoi(argv[i]);
-		mult = mult * k;
-	}
-	printf("%d\n", mult);
 	return (0);
-}
-/**
- *Description: _atoi - description for task.
- *@str: tring to be turned into an int
- *
- *Return: the value of the int
- */
-int _atoi(char *str)
-{
-	int res;
-	int i;
-	int sign;
-
-	sign = 1;
-	res = 0;
-	i = 0;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; str[i] != '\0';  i++)
-	{
-		res = (res * 10) + (str[i] - '0');
-	}
-	return (res * sign);
 }
