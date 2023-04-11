270b84b26fbdc70a9e8a88962a8b05e6a3fc94da
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-11 11:04:38 +0200

    Same message for automation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 /**
  *Description: main - description for task.
+ *
  *@argc: number of arguments for func
  *@argv: arguments
+ *
  *Return: 0 as a default return value
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
