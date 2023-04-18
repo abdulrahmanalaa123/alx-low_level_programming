af78b281550ddb3c4aad69180048f09b3bc0be51
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-18 17:42:55 +0200

    Same message for automation

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *Description: init_dog - description for task.
+ *@d: the object
+ *@name: naem fo
+ *@age: age of dgo
+ *@owner:owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
