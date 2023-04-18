d3e4ff6e462f0ff65fee02adc1a4eaca6ff10908
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-18 17:32:29 +0200

    Same message for automation

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# directory ofr structs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ *struct dog - dog class
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
