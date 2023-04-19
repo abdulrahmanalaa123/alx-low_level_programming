23a9c0dc4a5a8ffdb31cfef384047af1a99e0b3f
abdulrahmanalaa <abdulrahmanalaa497@gmail.com>
2023-04-19 04:36:11 +0200

    Same message for automation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,15 @@
+#include <stdlib.h>
 #include "dog.h"
+
 /**
- *Description: new_dog - description for task.
- *@name: name of dog
- *@age: age of dog
- *@owner: owner of dog
- *Return: 0 as a default return value
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
  */
-dog_t new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
